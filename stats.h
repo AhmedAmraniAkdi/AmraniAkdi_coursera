1d6a36792832043f7fee4854622eb8c36ea3b60a
Ahmed Amrani Akdi <ahmed-trabajos@hotmail.com>
2020-05-26 18:53:54 +0000

    Adding code outline and documentation for C1M1 Assessment

--- stats.h
+++ stats.h
@@ -1,40 +1,118 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header file for statistical analysis functions.
  *
- * <Add Extended Description Here>
+ * This file contains the following statistical analysis functions:
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * print_statistics() - A function that prints the statistics of an array including 
+ * minimum, maximum, mean, and median.
+ *
+ * print_array() - Given an array of data and a length, prints the array to the screen
+ *
+ * find_median() - Given an array of data and a length, returns the median value
+ *
+ * find_mean() - Given an array of data and a length, returns the mean
+ *
+ * find_maximum() - Given an array of data and a length, returns the maximum
+ *
+ * find_minimum() - Given an array of data and a length, returns the minimum
+ *
+ * sort_array() - Given an array of data and a length, sorts the array from largest to
+ * smallest. (The zeroth Element should be the largest value, and the last element (n-1)
+ * should be the smallest value. )
+ *
+ * @author Ahmed Amrani Akdi
+ * @date 26/05/2020
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
-
-/* Add Your Declarations and Function Comments here */ 
-
+ 
 /**
- * @brief <Add Brief Description of Function Here>
+ * Function : print_statistics
+ * Description : accepts a pointer to the array of the dataset and its size
+ * and prints all the statistical analysis of the dataset to the screen, namely, 
+ * the medan, the mean, the maximum and the minimum.
  *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @param int *arr : Pointer to array containing the dataset.
+ * @param int size : size of the array containing the dataset.
  */
+ 
+ void print_statistics(int* arr, int size);
+ 
+/**
+ * Function : print_array
+ * Description : accepts a pointer to the array of the dataset and its size
+ * and prints it to the screen.
+ *
+ * @param int *arr : Pointer to array containing the dataset.
+ * @param int size : size of the array containing the dataset.
+ */
+ 
+ void print_array(int* arr, int size);
+ 
+/**
+ * Function : find_median
+ * Description : accepts a pointer to the array of the dataset and its size
+ * and returns the median element of the dataset.
+ *
+ * @param int *arr : Pointer to array containing the dataset.
+ * @param int size : size of the array containing the dataset.
+ *
+ * @return int median : the median of the dataset.
+ */
+ 
+ int find_median(int* arr, int size);
+ 
+/**
+ * Function : print_statistics
+ * Description : accepts a pointer to the array of the dataset and its size
+ * and returns the mean value of the dataset.
+ *
+ * @param int *arr : Pointer to array containing the dataset.
+ * @param int size : size of the array containing the dataset.
+ *
+ * @return float mean : the mean of the dataset.
+ */
+ 
+ float mean(int* arr, int size);
+ 
+/**
+ * Function : print_statistics
+ * Description : accepts a pointer to the array of the dataset and its size
+ * and returns the maximum value of the dataset.
+ *
+ * @param int *arr : Pointer to array containing the dataset.
+ * @param int size : size of the array containing the dataset.
+ *
+ * @return int max : maximum value of the dataset.
+ */
+ 
+ int find_maximum(int* arr, int size);
+ 
+/**
+ * Function : print_statistics
+ * Description : accepts a pointer to the array of the dataset and its size
+ * and returns the minimum value of the dataset.
+ *
+ * @param int *arr : Pointer to array containing the dataset.
+ * @param int size : size of the array containing the dataset.
+ *
+ * @return int min : minimum value of the dataset.
+ */
+ 
+ int find_minimum(int* arr, int size);
+ 
+/**
+ * Function : print_statistics
+ * Description : accepts a pointer to the array of the dataset and its size
+ * and sorts it, the function uses selection sort algorithm.
+ *
+ * @param int *arr : Pointer to array containing the dataset.
+ * @param int size : size of the array containing the dataset.
+ */
+ 
+ void sort_array(int* arr, int size);
 
 
 #endif /* __STATS_H__ */
